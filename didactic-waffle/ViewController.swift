c01f1f9a10538ce1762a72c7bae5868b0d1ac8a1
Dauren <dauren.ktl@gmail.com>
2019-06-28 11:23:26 +0300

    remove Storyboard, ready for experiments :)

--- didactic-waffle/ViewController.swift
+++ didactic-waffle/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        print("ready for work :)")
     }
 
 
