6fb870562c994f884d0f6f54007a81277b5af57f
Dauren <dauren.ktl@gmail.com>
2019-06-28 11:13:42 +0300

    add the xcode project

---
+++ didactic-waffle/didactic-waffle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  didactic-waffle
+//
+//  Created by temp on 28/06/2019.
+//  Copyright © 2019 temp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
